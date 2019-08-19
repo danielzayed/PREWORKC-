ee678736090f5d3eea6e3834cce76487623c652d
danielzayed <53346422+danielzayed@users.noreply.github.com>
2019-08-19 16:29:36 +0300

    Delete local_library.h

--- local_library.h
+++
@@ -1,7 +0,0 @@
-#include "book.h"
-
-const Book * get_pointer() ;
-int get_num_Of_Books();
-char* get_name (long int _int_Book_Number);
-int get_Index(long int int_Book_Num);
-void specify_Genre_vars(Book * books) ;
